7a7a8b9145162afdfcc889402be5b001c2bc24b7
Ron Yutkin <ron@yutk.in>
2021-11-16 00:45:20 +0200

    finish version 1

--- ex5tests.py
+++ ex5tests.py
@@ -1,14 +1,20 @@
+import urllib.request
 import unittest
 from typing import Any
+import difflib
+import pprint
 
 import cartoonify as user
-
-VERSION = 1
 
 
 class CartoonifyTests(unittest.TestCase):
     MAX_DIFF_ALLOWED = 1
     EXPECTED_CHANGED_MSG = "You've changed the input matrix, which according to the PDF is ont allowed"
+    BETA_TEST_WARNING = '''
+    #######################
+    # THIS IS A BETA TEST #
+    #######################
+    '''
 
     def test__separate_channels__example_form_pdf(self):
         self.assertListEqual(
@@ -26,6 +32,20 @@ class CartoonifyTests(unittest.TestCase):
                 [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]],
             ),
             'Example from the forum: https://moodle2.cs.huji.ac.il/nu21/mod/forumng/discuss.php?d=2226&expand=1#p5642'
+        )
+
+    def test__separate_channels__NAME_ME(self):
+        self.assertListEqual(
+            [[[170, 45, 175], [251, 74, 79]],
+             [[20, 154, 28], [210, 105, 183]],
+             [[35, 161, 9], [134, 46, 139]]],
+            user.separate_channels(
+                [
+                    [[170, 20, 35], [45, 154, 161], [175, 28, 9]],
+                    [[251, 210, 134], [74, 105, 46], [79, 183, 139]]
+                ]
+            ),
+            'NAME ME'
         )
 
     def test__combine_channels__example_from_pdf(self):
@@ -46,6 +66,20 @@ class CartoonifyTests(unittest.TestCase):
             'Example from the forum: https://moodle2.cs.huji.ac.il/nu21/mod/forumng/discuss.php?d=2226&expand=1#p5642'
         )
 
+    def test__combine_channels__NAME_ME(self):
+        self.assertListEqual(
+            [
+                [[170, 20, 35], [45, 154, 161], [175, 28, 9]],
+                [[251, 210, 134], [74, 105, 46], [79, 183, 139]]
+            ],
+            user.combine_channels(
+                [[[170, 45, 175], [251, 74, 79]],
+                 [[20, 154, 28], [210, 105, 183]],
+                    [[35, 161, 9], [134, 46, 139]]]
+            ),
+            'NAME ME'
+        )
+
     def test__RGB2grayscale__example_from_pdf(self):
         input_ = [[[100, 180, 240]]]
         self.assertListEqual(
@@ -54,11 +88,34 @@ class CartoonifyTests(unittest.TestCase):
             'Example given on page 3'
         )
 
+    def test__RGB2grayscale__NAME_ME(self):
+        self.assertListAlmostEqual(
+            a1=[[67, 122, 70], [214, 89, 147]],
+            a2=user.RGB2grayscale(
+                [[[170, 20, 35], [45, 154, 161], [175, 28, 9]],
+                 [[251, 210, 134], [74, 105, 46], [79, 183, 139]]]
+            ),
+            msg='NAME ME',
+            delta=2
+        )
+
     def test__blur_kernel__example_from_pdf(self):
         self.assertListEqual(
             [[1/9, 1/9, 1/9], [1/9, 1/9, 1/9], [1/9, 1/9, 1/9]],
             user.blur_kernel(3),
             'Example given on page 3'
+        )
+
+    def test__blur_kernel__other_values(self):
+        self.assertListEqual(
+            [[1 / 25 for _ in range(5)] for _ in range(5)],
+            user.blur_kernel(5),
+            'Kernel size of 5'
+        )
+        self.assertListEqual(
+            [[1 / 49 for _ in range(7)] for _ in range(7)],
+            user.blur_kernel(7),
+            'Kernel size of 7'
         )
 
     def test__apply_kernel__example_from_pdf(self):
@@ -69,30 +126,63 @@ class CartoonifyTests(unittest.TestCase):
             'Example given on page 3'
         )
 
+    def test__apply_kernel__more_examples(self):
+        blur_kernel = [[0.04, 0.04, 0.04, 0.04, 0.04],
+                       [0.04, 0.04, 0.04, 0.04, 0.04],
+                       [0.04, 0.04, 0.04, 0.04, 0.04],
+                       [0.04, 0.04, 0.04, 0.04, 0.04],
+                       [0.04, 0.04, 0.04, 0.04, 0.04]]
+        self.assertListEqual(
+            [[79, 121, 82], [191, 96, 140]],
+            user.apply_kernel([[67, 122, 70], [214, 89, 147]], blur_kernel),
+            CartoonifyTests.BETA_TEST_WARNING
+        )
+
+    def test__apply_kernel__supports_generic_kernel(self):
+        kernel = [[0.01, 0.02, 0.03], [0.04, 0.05, 0.06], [0.07, 0.08, 0.09]]
+        self.assertListAlmostEqual(
+            [[47, 56, 41], [83, 48, 62]],
+            user.apply_kernel(
+                [[67, 122, 70], [214, 89, 147]], 
+                kernel
+            ),
+            CartoonifyTests.BETA_TEST_WARNING +
+            '\nAccording to https://moodle2.cs.huji.ac.il/nu21/mod/forumng/discuss.php?d=2288#p6063, apply_kernel should support any kernel'
+        )
+
     def test__bilinear_interpolation__example_from_pdf(self):
         image = [[0, 64], [128, 255]]
         self.assertAlmostEqual(
             0,
             user.bilinear_interpolation(image, 0, 0),
-            msg='Example 1 of 4 given on page 4, delta is 1',
+            msg='Example 1 of 4 given on page 4',
             delta=CartoonifyTests.MAX_DIFF_ALLOWED
         )
         self.assertAlmostEqual(
             255,
             user.bilinear_interpolation(image, 1, 1),
-            msg='Example 2 of 4 given on page 4, delta is 1',
+            msg='Example 2 of 4 given on page 4',
             delta=CartoonifyTests.MAX_DIFF_ALLOWED
         )
         self.assertAlmostEqual(
             112,
             user.bilinear_interpolation(image, 0.5, 0.5),
-            msg='Example 3 of 4 given on page 4, delta is 1',
+            msg='Example 3 of 4 given on page 4',
             delta=CartoonifyTests.MAX_DIFF_ALLOWED
         )
         self.assertAlmostEqual(
             160,
             user.bilinear_interpolation(image, 0.5, 1),
-            msg='Example 4 of 4 given on page 4, delta is 1',
+            msg='Example 4 of 4 given on page 4',
+            delta=CartoonifyTests.MAX_DIFF_ALLOWED
+        )
+
+    def test__bilinear_interpolation__more_tests_on_3by3(self):
+        image = [[67, 122, 70], [214, 89, 147]]
+        self.assertAlmostEqual(
+            114,
+            user.bilinear_interpolation(image, 1.3, 0.8),
+            msg='NAME ME',
             delta=CartoonifyTests.MAX_DIFF_ALLOWED
         )
 
@@ -118,6 +208,22 @@ class CartoonifyTests(unittest.TestCase):
             'Example 2/2 given on page 5'
         )
 
+    def test__rotate_90__more_examples(self):
+        matrix = [[[170, 20, 35], [45, 154, 161], [175, 28, 9]],
+                  [[251, 210, 134], [74, 105, 46], [79, 183, 139]]]
+        self.assertListEqual(
+            [[[251, 210, 134], [170, 20, 35]],
+             [[74, 105, 46], [45, 154, 161]],
+             [[79, 183, 139], [175, 28, 9]]],
+            user.rotate_90(matrix, 'R')
+        )
+        self.assertListEqual(
+            [[[175, 28, 9], [79, 183, 139]],
+             [[45, 154, 161], [74, 105, 46]],
+             [[170, 20, 35], [251, 210, 134]]],
+            user.rotate_90(matrix, 'L')
+        )
+
     def test__get_edges__example_from_pdf(self):
         self.assertListEqual(
             [[255, 0, 255]],
@@ -125,15 +231,63 @@ class CartoonifyTests(unittest.TestCase):
             'Example given on page 6'
         )
 
+    def test__get_edges__more_examples(self):
+        self.assertListEqual(
+            [[0, 255, 0], [255, 255, 255]],
+            user.get_edges([[67, 122, 70], [214, 89, 147]], 3, 5, 5)
+        )
+        self.assertListEqual(
+            [[0, 0, 255, 255, 0],
+             [255, 255, 255, 255, 255],
+             [255, 255, 0, 255, 255],
+             [255, 255, 0, 255, 255],
+             [255, 255, 0, 255, 255]],
+            user.get_edges(
+                [[67, 94, 122, 96, 70],
+                 [104, 109, 114, 102, 89],
+                 [140, 123, 106, 107, 108],
+                 [177, 137, 97, 112, 128],
+                 [214, 152, 89, 118, 147]], 1, 3, 5)
+        )
+
     def test__quantize__example_from_pdf(self):
         self.assertListAlmostEqual(
             [[0, 32, 96], [128, 191, 223]],
             user.quantize([[0, 50, 100], [150, 200, 250]], 8),
-            'Example given on page 7, delta is 1'
+            msg='Example given on page 7',
+            delta=CartoonifyTests.MAX_DIFF_ALLOWED
         )
 
-    def test__quantize_colored_image__(self):
-        pass
+    def test__quantize__example_from_pdf(self):
+        self.assertListAlmostEqual(
+            [[66, 120, 69], [212, 87, 145]],
+            user.quantize([[67, 122, 70], [214, 89, 147]], 100),
+            msg='Example given on page 7',
+            delta=CartoonifyTests.MAX_DIFF_ALLOWED
+        )
+
+    def test__quantize_colored_image__various_tests(self):
+        self.assertListAlmostEqual(
+            [[[159, 0, 32], [32, 128, 159], [159, 0, 0]],
+                [[223, 191, 128], [64, 96, 32], [64, 159, 128]]],
+            user.quantize_colored_image(
+                [[[170, 20, 35], [45, 154, 161], [175, 28, 9]],
+                 [[251, 210, 134], [74, 105, 46], [79, 183, 139]]],
+                8
+            )
+        )
+        self.assertListAlmostEqual(
+            [[[0, 8], [0, 20], [0, 28], [4, 40]],
+               [[4, 48], [4, 60], [4, 68], [8, 80]],
+               [[8, 88], [8, 100], [8, 108], [12, 120]],
+               [[12, 128], [12, 139], [12, 147], [16, 159]]],
+            user.quantize_colored_image(
+               [[[1, 10], [2, 20], [3, 30], [4, 40]],
+                [[5, 50], [6, 60], [7, 70], [8, 80]],
+                [[9, 90], [10, 100], [11, 110], [12, 120]],
+                [[13, 130], [14, 140], [15, 150], [16, 160]]], 64
+            )
+        )
 
     def test__add_mask__example_from_pdf(self):
         self.assertListEqual(
@@ -143,6 +297,11 @@ class CartoonifyTests(unittest.TestCase):
         )
 
     def test__add_mask__accepts_colored_image(self):
+        message = '''The instructions state that `add_mask` should be able to apply
+            a mask both single-channel (2d matrix) images and on multi-channel
+            (3d matrix) images (by separating the colored image to channels
+            and applying the mask)
+            Inspired by the example on page 9'''
         self.assertListEqual(
             [[[200, 200, 200], [125, 125, 125], [50, 50, 50]]],
             user.add_mask(
@@ -150,14 +309,37 @@ class CartoonifyTests(unittest.TestCase):
                 [[[200, 200, 200], [200, 200, 200], [200, 200, 200]]],
                 [[0, 0.5, 1]]
             ),
-            '''The instructions state that `add_mask` should be able to apply
-            a mask both single-channel (2d matrix) images and on multi-channel
-            (3d matrix) images (by separating the colored image to channels
-            and applying the mask)
-            Inspired by the example on page 9'''
+            message
+        )
+        self.assertListEqual(
+            [[[170, 20, 35], [24, 87, 82], [60, 29, 9]],
+             [[66, 82, 46], [19, 61, 29], [18, 81, 54]]],
+            user.add_mask(
+                [[[170, 20, 35], [45, 154, 161], [175, 28, 9]],
+                 [[251, 210, 134], [74, 105, 46], [79, 183, 139]]],
+                [[[1, 10, 1], [2, 20, 4], [3, 30, 9]],
+                 [[4, 40, 16], [5, 50, 25], [6, 60, 36]]],
+                [[1.0, 0.5, 0.33], [0.25, 0.2, 0.17]]
+            ),
+            message
+        )
+
+    def test__cartoonify__generic_test(self):
+        self.assertListAlmostEqual(
+            [[[0, 0, 0], [32, 128, 159], [0, 0, 0]],
+             [[223, 191, 128], [0, 0, 0], [64, 159, 128]]],
+            user.cartoonify(
+                [[[170, 20, 35], [45, 154, 161], [175, 28, 9]],
+                 [[251, 210, 134], [74, 105, 46], [79, 183, 139]]],
+                3, 3, 1, 8
+            )
         )
 
     def assertListAlmostEqual(self, a1, a2, delta=1, msg=None):
+        msg = msg if msg else ''
+        diffMsg = '\n' + '\n'.join(
+            difflib.ndiff(pprint.pformat(a1).splitlines(),
+                          pprint.pformat(a2).splitlines()))
         self.assertTrue(
             CartoonifyTests._are_almost_equal(
                 a1,
@@ -165,10 +347,10 @@ class CartoonifyTests(unittest.TestCase):
                 -1,
                 delta
             ),
-            msg
+            msg + '\nExpected then actual\n' + diffMsg
         )
 
-    @ staticmethod
+    @staticmethod
     def _are_almost_equal(o1: Any, o2: Any, max_abs_ratio_diff: float, max_abs_diff: float) -> bool:
         """
         Compares two objects by recursively walking them trough. Equality is as usual except for floats.
@@ -225,7 +407,7 @@ class CartoonifyTests(unittest.TestCase):
                    for v1, v2 in zip(o1, o2)):
                 return False
 
-        elif isinstance(o1, float):
+        elif isinstance(o1, (float)):
             if o1 == o2:
                 return True
             else:
@@ -248,5 +430,36 @@ class CartoonifyTests(unittest.TestCase):
         return True
 
 
+def version_check():
+    up_to_date = True
+    req = urllib.request.Request(
+        url=VERSION_CHECK, method='GET'
+    )
+    try:
+        with urllib.request.urlopen(req) as resp:
+            version = resp.read().decode(
+                resp.headers.get_content_charset("utf-8")
+            )
+        up_to_date = int(version) <= VERSION
+    except:
+        print('Could not check for updates')
+        up_to_date = True
+    return up_to_date
+
+
+def main():
+    if version_check():
+        print('Up to date, running tests...')
+        print("If a test has failed when it shouldn't have, please contact me at yutkin@cs.huji.ac.il or open an issue or a pull request here: https://github.com/TwoUnderscorez/huji_intro2cs1_ex5")
+        unittest.main()
+    else:
+        print("We've updated the tests file, please download the new one form here")
+        print('https://raw.githubusercontent.com/TwoUnderscorez/huji_intro2cs1_ex5/master/ex5tests.py')
+
+
+# .data
+VERSION = 1
+VERSION_CHECK = 'https://raw.githubusercontent.com/TwoUnderscorez/huji_intro2cs1_ex5/master/VERSION'
+
 if __name__ == '__main__':
-    unittest.main()
+    main()
